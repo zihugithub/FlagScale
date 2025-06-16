da2e3b1028ee3c6c00e9d54d9540dbab4445a73f
yefubao <fbye@baai.ac.cn>
2025-06-16 17:10:01 +0800

    update test.time_statistics.py

--- tests/scripts/test.time_statistics.py
+++ tests/scripts/test.time_statistics.py
@@ -17,7 +17,7 @@ unit = {
     "name": "单元测试指令运行时间统计",
     "image": "镜像",
     'backend': [ "megatron", "flagscale" ],
-    "subset": { 
+    "subset": {
         "megatron": ["data", "dist_checkpointing", "distributed", "export", "fusions", "inference",
                "models", "pipeline_parallel", "post_training", "ssm", "tensor_parallel", "transformer/moe",
                "transformer", "./", "", "" ],
@@ -46,7 +46,7 @@ functional = {
         },
         "train": {
              "aquila": [ "tp2_pp2", "tp4_pp2" ],
-             "deepseek": [ "tp2_pp2_ep2" ], 
+             "deepseek": [ "tp2_pp2_ep2" ],
              "mixtral": [ "tp2_pp1_ep2", "tp4_pp1_ep2" ],
              "llava_onevision": [ "tp2", "tp4" ]
         } ,
@@ -56,6 +56,18 @@ functional = {
         "serve": {
             "base": [ "multiple_model" ]
         }
+    },
+
+    "new_functional": {
+        "type": [ "inference-pipeline" ],
+        "layout": {
+            "inference-pipeline": {
+                "task": [ "Qwen3-4B" ],
+                "hardware": [ "nvidia", "bi_v150", "cambricon_mlu" ],
+                "flaggems": [ "enable", "disable" ],
+                "case_runtime": [ "nvidia", "nvidia_flaggems" ]
+            },
+        },
     }
 }
 
@@ -75,9 +87,9 @@ class test_time_statistics(object):
           eg: >>>: train_aquila_tp2_pp2 runtime: 2
         '''
         for root,dirs,files in os.walk(os.getcwd()):
-            if self.file_name_txt in files: 
+            if self.file_name_txt in files:
                 self.file_name_txt = os.path.join(root,self.file_name_txt)
-
+            
         assert os.path.isfile(self.file_name_txt), f"请先将测试用例运行时间存于文件：{self.xlsx_path()}/{self.file_name_txt}"
         return self.file_name_txt
 
@@ -117,6 +129,14 @@ class test_time_statistics(object):
 
         return ci_image
 
+    def get_cpuANDgpu(self):
+        gpu_models = set(os.popen('nvidia-smi --query-gpu=name --format=csv,noheader').read().strip().split("\n"))
+        gpu_processor = os.popen('nvidia-smi -L | wc -l').read().strip()
+        cpu_model = os.popen('cat /proc/cpuinfo | grep "model name" | head -1').read().split(":")[1].strip()
+        cpu_processor = os.popen('cat /proc/cpuinfo | grep "processor" | wc -l').read().strip()
+        cpuANDgpu = "gpu_model :{}\ngpu_processor :{}\ncpu_model :{}\ncpu_processor :{}".format(gpu_models,gpu_processor,cpu_model,cpu_processor)
+        return cpuANDgpu
+
     def create_unit_layout(self):
         wb = Workbook()
         ws = wb.active
@@ -146,8 +166,8 @@ class test_time_statistics(object):
             for megatron_index1, megatron_field in enumerate(unit["subset"]["megatron"]):
                 if megatron_index0 == megatron_index1:
                     ws["B%s"%(megatron_site)] = megatron_field
-                    ws["D%s"%(megatron_site)] = 2  # eg
-        ws["D%s"%(megatron_end_index)] = "none"    # eg
+        #             ws["D%s"%(megatron_site)] = 2  # eg
+        # ws["D%s"%(megatron_end_index)] = "none"    # eg
 
         # Subset Flagscale Field Arrangement
         for flagscale_index0, flagscale_site in enumerate(range(megatron_end_index + 1, megatron_end_index + len(unit["subset"]["flagscale"]) + 1)):
@@ -157,8 +177,8 @@ class test_time_statistics(object):
             for flagscale_index1, flagscale_field in enumerate(unit["subset"]["flagscale"]):
                 if flagscale_index0 == flagscale_index1:
                     ws["B%s"%(flagscale_site)] = flagscale_field
-                    ws["D%s"%(flagscale_site)] = 2  # eg
-        ws["D%s"%(flagscale_end_index)] = "none"    # eg
+        #             ws["D%s"%(flagscale_site)] = 2  # eg
+        # ws["D%s"%(flagscale_end_index)] = "none"    # eg
         ws["D2"] = "flagscale:cuda12.4.1-cudnn9.5.0-python3.12-torch2.6.0-time2505241715"
 
         # backend megatron Field Arrangement
@@ -178,7 +198,11 @@ class test_time_statistics(object):
         ws.merge_cells("C{}:C{}".format(flagscale_start_index,flagscale_end_index-2))
         ws["C%s"%(flagscale_end_index-1)] = unit["status"][0]
         ws["C%s"%(flagscale_end_index)] = unit["status"][1]
-        # eg
+
+        # The quantity and model of CPUs and GPUs
+        ws.merge_cells("A{}:C{}".format(flagscale_end_index+1,flagscale_end_index+1))
+        ws["A{}".format(flagscale_end_index+1)] = "CPU | GPU 型号与数量："
+
         wb.save(f"{self.xlsx_path()}/{self.file_name_xlsx}")
 
     def create_functional_layout(self):
@@ -265,6 +289,13 @@ class test_time_statistics(object):
                 if case_runtime_index0 == case_runtime_index1:
                     ws["C%s"%(case_runtime_site)] = case_runtime_field
 
+
+        # The quantity and model of CPUs and GPUs
+        ws.merge_cells("A{}:C{}".format(serve_end_index+1,serve_end_index+1))
+        ws["A{}".format(serve_start_index+1)] = "CPU | GPU 型号与数量："
+
+
+
         wb.save(f"{self.xlsx_path()}/{self.file_name_xlsx}")
 
     def get_line_range(self):
@@ -300,7 +331,7 @@ class test_time_statistics(object):
             if letter0 == letter1 and letter1 == "A" and int(number1) >= 4:
                 backend = ws[min_sheet0].value
                 for sub_set_index in range(int(number0), int(number1) + 1):
-                    subset = ws[f"B{sub_set_index}"].value    
+                    subset = ws[f"B{sub_set_index}"].value
                     com_key_xlsx = "_".join([str(backend), str(subset)])
 
         with open(self.data_file(),"r") as f:
@@ -342,13 +373,13 @@ class test_time_statistics(object):
 
         com_key_xlsx_list = {} # 从 xlsx 中获取行号(key) 及 case_runtime
         for merged_cells in ws.merged_cells:
-            if len(merged_cells.coord.split(":")) == 1:  # ['A12'] 
+            if len(merged_cells.coord.split(":")) == 1:  # ['A12']
                 max_sheet0 = merged_cells.coord.split(":")[0]  # A12
                 letter0 = "".join([char for char in max_sheet0 if char.isalpha()])  # A
                 number0 = "".join([char for char in max_sheet0 if char.isdigit()])  # 12
                 if letter0 == "A" and int(number0) >= 3:
                     _type = ws[max_sheet0].value      # server
-                    _task = ws[f"B{number0}"].value   # base            
+                    _task = ws[f"B{number0}"].value   # base
                     _case_runtime = ws[f"C{number0}"].value.split("\n")  # ['multiple_model:']
                     for i in range(0, len(_case_runtime)):
                         com_key_xlsx = "_".join([str(_type), str(_task), str(_case_runtime[i])]) # serve_base_multiple_model
@@ -377,7 +408,7 @@ class test_time_statistics(object):
             cases_runtime = [] # ['train_aquila_tp2_pp2: 1', 'train_aquila_tp4_pp2: 2']
             for line in lines:
                 if "runtime" in line:
-                    runtime = "".join(f"{line.split(r' ')[1].strip()}: {line.split(r' ')[3].strip()}") 
+                    runtime = "".join(f"{line.split(r' ')[1].strip()}: {line.split(r' ')[3].strip()}")
                     cases_runtime.append(runtime)
 
         site_and_field = {}
@@ -400,34 +431,113 @@ class test_time_statistics(object):
 
         start_case_runtime_site = self.get_line_range()[0]
         end_case_runtime_site = self.get_line_range()[1]
-        for activate_site in range(start_case_runtime_site, end_case_runtime_site + 1):
+        for activate_site in range(start_case_runtime_site, end_case_runtime_site):
             if ws[f"{action_column}{activate_site}"].value == None :
                 ws[f"{action_column}{activate_site}"] = "none"
 
         wb.save(f"{self.xlsx_path()}/{self.file_name_xlsx}")
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: test.time_statistics.py <test_type>")
-        sys.exit(1)
-    test_type = sys.argv[1]
+    def append_functional_layout(self):
+        '''
+        添加功能测试用例
+        '''
+        wb = load_workbook(f"{self.xlsx_path()}/{self.file_name_xlsx}")
+        ws = wb.active
+        # 确定加入位置及范围
+        join_start_site = self.get_line_range()[1]
+        for type in functional["new_functional"]["type"]:
+            if type == "inference-pipeline":
+                inference_pipeline_range = len(functional["new_functional"]["layout"]["inference-pipeline"]["hardware"]) * len(functional["new_functional"]["layout"]["inference-pipeline"]["flaggems"])
+                # 在第二列之后添加2列
+                insert_column = 2 + 1 
+                num_columns_to_insert = 2
+                ws.insert_cols(insert_column, amount=num_columns_to_insert)
+                ws["C2"] = "hardware"
+                ws["D2"] = "flaggems"
 
-    # test_type = "functional"
+                # 添加行
+                ws.insert_rows(join_start_site, amount=inference_pipeline_range)
+
+                # type inference-pipeline Field Arrangement
+                ws.merge_cells("A{}:A{}".format(join_start_site, join_start_site + inference_pipeline_range - 1))
+                ws["A{}".format(join_start_site)] = "inference-pipeline"
+                # task Field Arrangement
+                for tk_site in range(join_start_site, join_start_site + inference_pipeline_range):
+                    ws["B{}".format(tk_site)] = "Qwen3-4B"
+                # hardware Field Arrangement
+                for hd in functional["new_functional"]["layout"]["inference-pipeline"]["hardware"]:
+                    if hd == "nvidia":
+                        ws["C{}".format(join_start_site)] = hd
+                        ws["C{}".format(join_start_site+1)] = hd
+                        ws["D{}".format(join_start_site+1)] = "enable"
+                    elif hd == "bi_v150":
+                        ws["C{}".format(join_start_site+2)] = hd
+                        ws["C{}".format(join_start_site+3)] = hd
+                        ws["D{}".format(join_start_site+3)] = "enable"
+                    elif hd == "cambricon_mlu":
+                        ws["C{}".format(join_start_site+4)] = hd
+                        ws["C{}".format(join_start_site+5)] = hd
+                        ws["D{}".format(join_start_site+5)] = "enable"
+
+        wb.save(f"{self.xlsx_path()}/{self.file_name_xlsx}")
+
+    def functional_append_data(self):
+        # wb = load_workbook(f"{self.xlsx_path()}/{self.file_name_xlsx}")
+        # ws = wb.active
+        with open(self.data_file(),"r") as f:
+            lines = f.readlines()
+            cases_runtime = [] # ['inference-pipeline_Qwen3-4B_nvidia: 77', 'inference-pipeline_Qwen3-4B_nvidia_flaggems: 90']
+            for line in lines:
+                if "runtime" in line:
+                    runtime = "".join(f"{line.split(r' ')[1].strip()}: {line.split(r' ')[3].strip()}")
+                    cases_runtime.append(runtime)
+        # for merged_cells in ws.merged_cells:
+        
+        #     print(merged_cells)
+            # for rows in ws.iter_rows():
+            #     for row in rows:
+            #         if row.value == "inference-pipeline":
+            #             print(row)
+            #         elif row.value is None:
+            #             continue
+            # print(ws["A13"].value)
+            
+        # for case_runtime in cases_runtime: # 'serve_base_multiple_model: 2'
+        #     case_runtime0 = case_runtime.split(r' ')[0].strip()
+        #     case_runtime1 = case_runtime.split(r' ')[1].strip()
+        #     if field0 == case_runtime0:
+        #         site_and_field.setdefault(key_site,[]).append(f"{field1} {case_runtime1}")
+
+        # print(cases_runtime)
+        # wb.save(f"{self.xlsx_path()}/{self.file_name_xlsx}")
+
+if __name__ == "__main__":
+    # if len(sys.argv) != 2:
+    #     print("Usage: test.time_statistics.py <test_type>")
+    #     sys.exit(1)
+    # test_type = sys.argv[1]
+
+    test_type = "functional"
     # test_type = "unit"
 
     case = test_time_statistics(test_type)
+    case.create_functional_layout()
+    case.append_functional_layout()
+    case.functional_append_data()
+    # case.get_line_range()
+    # case.functional_add_data()
 
-    if os.path.isfile(case.file_name_xlsx):
-        if test_type == "unit":
-            case.unit_add_data()
-        elif test_type == "functional":
-            case.functional_add_data()
-    else:
-        if test_type == "unit":
-            case.create_unit_layout()
-            case.get_line_range()
-            case.unit_add_data()
-        elif test_type == "functional":
-            case.create_functional_layout()
-            case.get_line_range()
-            case.functional_add_data()
+    # if os.path.isfile(case.file_name_xlsx):
+    #     if test_type == "unit":
+    #         case.unit_add_data()
+    #     elif test_type == "functional":
+    #         case.functional_add_data()
+    # else:
+        # if test_type == "unit":
+        #     case.create_unit_layout()
+        #     case.get_line_range()
+        #     case.unit_add_data()
+        # elif test_type == "functional":
+        #     case.create_functional_layout()
+        #     case.get_line_range()
+        #     case.functional_add_data()
